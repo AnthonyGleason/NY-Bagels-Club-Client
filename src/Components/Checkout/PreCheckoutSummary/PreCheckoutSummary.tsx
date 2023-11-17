15eb62ed173195809e5f3869ed21b0e4e62b9278
Anthony Infortunio <contact@anthonygleason.com>
2023-11-17 11:17:22 -0500

    update client

--- src/Components/Checkout/PreCheckoutSummary/PreCheckoutSummary.tsx
+++ src/Components/Checkout/PreCheckoutSummary/PreCheckoutSummary.tsx
@@ -61,56 +61,58 @@ export default function PreCheckoutSummary({
 
   return(
     <section className='cart-summary' onClick={()=>{setIsSidebarExpanded(isSidebarExpanded===true ? false: false)}}>
-      <h3>Basket</h3>
-      <table>
-        <thead>
-          <tr>
-            <th className="item-name">Name</th>
-            <th className="item-quantity">Quantity</th>
-            <th className="item-subtotal">Subtotal</th>
-            <th className="item-remove">Remove</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            cart.items.map((cartItem, index) => {
-              return (
-                <CartSummaryItem 
-                  key={index}
-                  cartItem={cartItem}
-                  setCart={setCart}
-                  isCheckoutView={isCheckoutView}
-                />
-              );
-            })
-          }
-        </tbody>
-      </table>
-      <div className='cart-subtotal'>
-        <span><strong>Basket Subtotal: ${cart.subtotalInDollars.toFixed(2)}</strong></span>
+      <div className='cart-summary-wrapper'>
+        <h3>Basket</h3>
+        <table>
+          <thead>
+            <tr>
+              <th className="item-name">Name</th>
+              <th className="item-quantity">Quantity</th>
+              <th className="item-subtotal">Subtotal</th>
+              <th className="item-remove">Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              cart.items.map((cartItem, index) => {
+                return (
+                  <CartSummaryItem 
+                    key={index}
+                    cartItem={cartItem}
+                    setCart={setCart}
+                    isCheckoutView={isCheckoutView}
+                  />
+                );
+              })
+            }
+          </tbody>
+        </table>
+        <div className='cart-subtotal'>
+          <span><strong>Basket Subtotal: ${cart.subtotalInDollars.toFixed(2)}</strong></span>
+        </div>
+        <div className='calendar-wrapper'>
+          <h3>Choose Your Ship Date</h3>
+          <Calendar 
+            value={date} 
+            onChange={(selectedDate:any)=>{setDate(selectedDate)}}
+            tileDisabled={({date}) => ![3, 4].includes(date.getDay())}
+            minDetail='month'
+            maxDetail='month'
+            minDate={getTomorrowDate()}
+          />
+          <strong>
+            {
+              date ?
+                `Your order will ship on ${date.toDateString()} or the next available business day.`
+              :
+                'Please select a Wednesday or Thursday you would like your order shipped on.'
+            }
+          </strong>
+        </div>
+        <b className='cart-shipping-note'>Note: Shipping and taxes are calculated at checkout.</b>
+        {/* <button onClick={()=>{alert("We appreciate your interest in our delicious bagels! Although we're not officially open yet, we're still accepting orders. Feel free to contact sales@nybagelsclub.com to place any orders.")}}>Checkout</button> */}
+        <button className='button-styled' onClick={()=>{handleNavigateCheckout()}}>Checkout Now</button>
       </div>
-      <div className='calendar-wrapper'>
-        <h3>Choose Your Ship Date</h3>
-        <Calendar 
-          value={date} 
-          onChange={(selectedDate:any)=>{setDate(selectedDate)}}
-          tileDisabled={({date}) => ![3, 4].includes(date.getDay())}
-          minDetail='month'
-          maxDetail='month'
-          minDate={getTomorrowDate()}
-        />
-        <strong>
-          {
-            date ?
-              `Your order will ship on ${date.toDateString()} or the next available business day.`
-            :
-              'Please select a Wednesday or Thursday you would like your order shipped on.'
-          }
-        </strong>
-      </div>
-      <b className='cart-shipping-note'>Note: Shipping and taxes are calculated at checkout.</b>
-      {/* <button onClick={()=>{alert("We appreciate your interest in our delicious bagels! Although we're not officially open yet, we're still accepting orders. Feel free to contact sales@nybagelsclub.com to place any orders.")}}>Checkout</button> */}
-      <button className='button-styled' onClick={()=>{handleNavigateCheckout()}}>Checkout</button>
     </section>
   )
 };
