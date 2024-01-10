fb83dd06925ccc2de484e7eb0f4e2b6f55ce271f
Anthony Infortunio <contact@anthonygleason.com>
2024-01-10 17:18:31 -0500

    remove paragraph infront of bagel pictures, add padding to bagel image

--- src/Components/Home/About/About.tsx
+++ src/Components/Home/About/About.tsx
@@ -5,14 +5,14 @@ export default function About(){
   return(
     <section className='about-us'>
       <img decoding='async' loading='lazy' src={window.innerWidth <= 450 ? './Assets/about-mobile.webp' : aboutDesktopImg} alt='delicious gourmet new york city bagels' />
-      <motion.p
+      {/* <motion.p
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{duration: 2.5, ease: easeInOut }}
         viewport={{once: false}}
       >
         Experience the genuine flavors of New York with our expertly hand-rolled, kettled, and freshly baked New York Bagels. Crafted with the finest ingredients, precision, and dedication, Brendel's gourmet delights are cherished across Long Island, New York, for their exceptional quality. To ensure maximum freshness upon arrival at your doorstep, we carefully vacuum-seal all bagels in every order, preserving the distinctive New York flavor. Whether you're a former New Yorker, hosting family gatherings, or organizing a corporate event, choose New York Bagels Club for gourmet cuisine and top-tier catering services.
-      </motion.p>
+      </motion.p> */}
     </section>
   )
 }
