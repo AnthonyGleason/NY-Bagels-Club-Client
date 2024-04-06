29681a39ad1017dd9f15c5db89d02b176dfe691b
Anthony Infortunio <contact@anthonygleason.com>
2024-04-06 13:55:02 -0400

    remove club, refactoring

--- src/Components/Home/Hero/Hero.tsx
+++ src/Components/Home/Hero/Hero.tsx
@@ -37,7 +37,7 @@ export default function Hero(){
         <p>
           To meet the highest standards of freshness, our hand-rolled bagels are kettled and vacuum-sealed just two hours after leaving the oven, ensuring we seal in the perfect flavor of a delicious New York Bagel.
         </p>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3019.1385204811727!2d-73.20765392337144!3d40.82492097137684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e830bdf003da29%3A0x762920bc7ed49416!2sBrendel&#39;s%20Bagels-Catering%20Long%20Island!5e0!3m2!1sen!2sus!4v1712261072293!5m2!1sen!2sus" width={'100%'} height="400" allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3019.755572565583!2d-73.508357323218!3d40.8113647315256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c283781bd4bf5b%3A0x5243486a991bb12b!2sBrendel&#39;s%20Bagels-Catering%20Long%20Island!5e0!3m2!1sen!2sus!4v1712425568917!5m2!1sen!2sus" width={'100%'} height="400" allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
       </article>
       <img
         decoding='async'
